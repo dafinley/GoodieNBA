604ab975e62e2894ad05637e71e7bbc9b20043d0
COR Students <corstudents@Matts-MBP-6.localdomain>
2019-02-09 13:50:03 -0500

    working mouse events

--- src/App.js
+++ src/App.js
@@ -3,14 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+constructor(props){
+    super(props);
+    this.state = {marker: {x:0,y:0}};
+}
+
+showCoords(event) {
+  this.setState({ marker: { x: event.screenX, y: event.screenY}});
+  var coords = "X coords: " + this.state.marker.x + ", Y coords: " + this.state.marker.y;
+  console.log(this.marker);
+  //alert(coords);
+}
+
   render() {
+    
     return (
       <div className="App">
        <h1>Hello World!</h1>
-       { <img style={{height:"550px"}} img src={require('./Images/court.png')} alt=''/>   }
+       <div>
+         <div>X={this.state.marker.x}</div>
+         <div>Y={this.state.marker.y}</div>
+      </div>
+       <img style={{height:"550px"}} onClick={ (event) => this.showCoords(event) } img src={require('./Images/court.png')} alt=''/>
       </div>
     );
-  }
-}
+  };
+};
 
 export default App;
