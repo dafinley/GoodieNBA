d2549d1c332264d7fcae55d46b92bc75fbd315c5
COR Students <corstudents@Matts-MBP-6.localdomain>
2019-02-09 16:14:20 -0500

    add all shots

---
+++ src/Components/AllShots.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+
+class AllShots extends Component {
+ 
+    state = { data: [] };
+    URL = 'https://goodie-nba.starlineventures.com/api/allShots';
+    componentDidMount() {
+        // when component mounted, start a GET request
+        // to specified URL
+        fetch(URL)
+          // when we get a response map the body to json
+          .then(response => response.json())
+          // and update the state data to said json
+          .then(data => this.setState({ data }));
+      }
+
+    render() {
+
+        <ul>
+        {
+          this.state.data.map(function(record){
+            return <li key={record.id}>{record.name} - {record.x} { record.y } { record.madeMiss}</li>;
+          })
+        }
+        </ul>
+
+    }
+
+}
+
+export default AllShots;
