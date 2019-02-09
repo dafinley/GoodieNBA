9aaf90d41336fe06a6f42428cc1b74b1b614a3f3
COR Students <corstudents@Matts-MBP-62.localdomain>
2019-02-09 15:31:34 -0500

    modal-inserted

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import Popup from './Components/Popup'
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       <h1>Hello World!</h1>
-       { <img style={{height:"550px"}} img src={require('./Images/court.png')} alt=''/>   }
-      </div>
-    );
-  }
+    render(){
+        return(
+            <div>
+                        <img style={{ height: "550px" }} img src={require('./Images/court.png')} alt='' />
+
+                <div>
+NBA ALLSTAR                <Popup/>
+            </div>
+        </div>
+        )
+    }
 }
 
-export default App;
+export default App
---
+++ src/Components/Test.js
@@ -0,0 +1,27 @@
+// import React, {Component} from 'react'
+
+// class Test extends Component {
+//     render(){
+//         return(
+//             <div>
+//                 <h1>Testing?</h1>
+
+// //   render() {
+//     return (
+
+//       <div></div>
+
+//       <div>
+//         <h1>Hello World!</h1>
+//         <Button>lkjlk</Button>
+//         <img style={{ height: "550px" }} img src={require('./Images/court.png')} alt='' />
+//       </div>
+//     );
+//   }
+// }
+//             </div>
+//         )
+//     }
+// }
+
+// export default Test
